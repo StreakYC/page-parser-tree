8c30411157f6aeaeb05c65b2721fa2d5e65a2b69
Chris Cowan <agentme49@gmail.com>
2017-02-17 14:22:25 -0800

    Split function to own file.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 /* @flow */
 
-import t from 'transducers.js';
 import LiveSet from 'live-set';
 import type {LiveSetController, LiveSetSubscription} from 'live-set';
-import liveSetTransduce from 'live-set/transduce';
 import liveSetMerge from 'live-set/merge';
-import liveSetFilter from 'live-set/filter';
 import liveSetFlatMap from 'live-set/flatMap';
 import liveSetMap from 'live-set/map';
 import {TagTree} from 'tag-tree';
 import type {TagTreeController, TagTreeNode} from 'tag-tree';
-import matchesSelector from 'matches-selector-ng';
 
-import makeElementChildLiveSet from './makeElementChildLiveSet';
 import watcherFinderMerger from './watcherFinderMerger';
+import makeLiveSetTransformerFromSelectors from './makeLiveSetTransformerFromSelectors';
 
 export type Selector =
   string
@@ -80,54 +76,6 @@ export type ElementContext = {|
   el: HTMLElement;
   parents: Array<NodeTagPair>;
 |};
-
-function makeLiveSetTransformer(selectors: Array<Selector>): (liveSet: LiveSet<ElementContext>) => LiveSet<ElementContext> {
-  const transformers = selectors.map(item => {
-    if (typeof item === 'string') {
-      const itemString = item;
-      const filterXf = t.filter(el => matchesSelector(el, itemString));
-      const flatMapFn = ec => {
-        const transducer = t.compose(
-          filterXf,
-          t.map(el => ({el, parents: ec.parents}))
-        );
-        return liveSetTransduce(makeElementChildLiveSet(ec.el), transducer);
-      };
-      return liveSet => liveSetFlatMap(liveSet, flatMapFn);
-    } else if (item.$or) {
-      const transformers = item.$or.map(makeLiveSetTransformer);
-      return liveSet =>
-        liveSetMerge(transformers.map(transformer =>
-          transformer(liveSet)
-        ));
-    } else if (item.$watch) {
-      throw new Error('TODO');
-    } else if (item.$log) {
-      const {$log} = item;
-      const filterFn = value => {
-        console.log($log, value.el); //eslint-disable-line no-console
-        return true;
-      };
-      return liveSet => liveSetFilter(liveSet, filterFn);
-    } else if (item.$filter) {
-      const {$filter} = item;
-      const filterFn = ({el}) => $filter(el);
-      return liveSet => liveSetFilter(liveSet, filterFn);
-    } else if (item.$map) {
-      const {$map} = item;
-      const transducer = t.compose(
-        t.map(ec => ({el: $map(ec.el), parents: ec.parents})),
-        t.filter(ec => ec.el != null)
-      );
-      return liveSet => liveSetTransduce(liveSet, transducer);
-    }
-    throw new Error(`Invalid selector item: ${JSON.stringify(item)}`);
-  });
-
-  return transformers.reduce((combined, transformer) => {
-    return liveSet => transformer(combined(liveSet));
-  }, x => x);
-}
 
 function makeTagOptions(options: PageParserTreeOptions) {
   const map = new Map();
@@ -270,7 +218,7 @@ export default class PageParserTree {
         return entry.ecSet;
       });
       const sourceSet = sourceSets.length === 1 ? sourceSets[0] : liveSetMerge(sourceSets);
-      const transformer = makeLiveSetTransformer(selectors);
+      const transformer = makeLiveSetTransformerFromSelectors(selectors);
 
       const ecEntry = this._ecSources.get(tag);
       if (!ecEntry) throw new Error();
---
+++ src/makeLiveSetTransformerFromSelectors.js
@@ -0,0 +1,59 @@
+/* @flow */
+
+import t from 'transducers.js';
+import matchesSelector from 'matches-selector-ng';
+import liveSetTransduce from 'live-set/transduce';
+import liveSetFilter from 'live-set/filter';
+import liveSetMerge from 'live-set/merge';
+import liveSetFlatMap from 'live-set/flatMap';
+import makeElementChildLiveSet from './makeElementChildLiveSet';
+import type LiveSet from 'live-set';
+import type {Selector, ElementContext} from './index';
+
+export default function makeLiveSetTransformerFromSelectors(selectors: Array<Selector>): (liveSet: LiveSet<ElementContext>) => LiveSet<ElementContext> {
+  const transformers = selectors.map(item => {
+    if (typeof item === 'string') {
+      const itemString = item;
+      const filterXf = t.filter(el => matchesSelector(el, itemString));
+      const flatMapFn = ec => {
+        const transducer = t.compose(
+          filterXf,
+          t.map(el => ({el, parents: ec.parents}))
+        );
+        return liveSetTransduce(makeElementChildLiveSet(ec.el), transducer);
+      };
+      return liveSet => liveSetFlatMap(liveSet, flatMapFn);
+    } else if (item.$or) {
+      const transformers = item.$or.map(makeLiveSetTransformerFromSelectors);
+      return liveSet =>
+        liveSetMerge(transformers.map(transformer =>
+          transformer(liveSet)
+        ));
+    } else if (item.$watch) {
+      throw new Error('TODO');
+    } else if (item.$log) {
+      const {$log} = item;
+      const filterFn = value => {
+        console.log($log, value.el); //eslint-disable-line no-console
+        return true;
+      };
+      return liveSet => liveSetFilter(liveSet, filterFn);
+    } else if (item.$filter) {
+      const {$filter} = item;
+      const filterFn = ({el}) => $filter(el);
+      return liveSet => liveSetFilter(liveSet, filterFn);
+    } else if (item.$map) {
+      const {$map} = item;
+      const transducer = t.compose(
+        t.map(ec => ({el: $map(ec.el), parents: ec.parents})),
+        t.filter(ec => ec.el != null)
+      );
+      return liveSet => liveSetTransduce(liveSet, transducer);
+    }
+    throw new Error(`Invalid selector item: ${JSON.stringify(item)}`);
+  });
+
+  return transformers.reduce((combined, transformer) => {
+    return liveSet => transformer(combined(liveSet));
+  }, x => x);
+}
